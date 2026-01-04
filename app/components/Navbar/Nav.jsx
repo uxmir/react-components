cf3c3e68720d2f7a2b64686f949c2bdc4e5613c5
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-01-05 01:02:55 +0600

    page transition animation 1 done

--- app/components/Navbar/Nav.jsx
+++ app/components/Navbar/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='flex gap-x-3 text-2xl mt-5'>
+    <div className='flex gap-x-3 text-2xl fixed top-10'>
       <Link href={'/'}>home</Link> 
          <Link href={'/product'}>product</Link> 
             <Link href={'/about'}>about</Link> 
--- app/components/PageTransition/PageTransition1.jsx
+++ app/components/PageTransition/PageTransition1.jsx
@@ -7,51 +7,74 @@ const PageTransition1 = ({ children }) => {
   const pathName = usePathname();
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
-        gsap.fromTo(
-          ".transition-container",
-          {
-            height: "100%",
+    const tl=gsap.timeline()
+    tl.to([".image-left",".image-right"],{
+     x:(index)=>index===0?'-120%':'120%',
+     duration:0.7,
+     delay:0.8,
+    })
+    .fromTo([".image-left",".image-right"],{
+    clipPath: "inset(0% 0% 0% 0%)",
+    },{
+      clipPath: "inset(0% 0% 100% 0%)",
+      ease: "sine.inOut",
+      duration:2,
+    })
+    .fromTo(".image-middle",{
+      clipPath: "inset(0% 0% 0% 0%)",
+    },{
+       clipPath: "inset(100% 0% 0% 0%)",
+        ease: "sine.inOut",
+        duration: 2,
+    })
+    .fromTo(".transition-container",  {
+            y: "0%",
           },
           {
-            height: "0%",
+            y: "-100%",
             ease: "sine.inOut",
-            duration: 8,
+            duration: 2,
           }
-        );
-      gsap.fromTo('.image-left',{
-      clipPath: "inset(0% 0% 0% 0%)",
-      },
-      {
-       clipPath: "inset(0% 0% 100% 0%)",
-        ease: "sine.inOut",
-        duration:4,
-      }
-    )
-         gsap.fromTo('.image-right',{
-      clipPath: "inset(0% 0% 0% 0%)",
-      },
-      {
-       clipPath: "inset(0% 0% 100% 0%)",
-        ease: "sine.inOut",
-        duration: 4,
-      }
+
     )
     });
     return () => ctx.revert();
   }, [pathName]);
   return (
-    <div className="fixed w-full h-full transition-container  top-0 left-0 right-0 z-[9999] bg-black">
+  <>
+    <div className="fixed w-full h-full transition-container pointer-events-none  top-0 left-0 right-0  bg-black">
       <Container>
         <div className="flex justify-center items-center mt-50">
-          <div className="flex gap-x-10 items-center">
-            <div className="w-70 h-90 image-left "><img src="/myphoto 1.png" alt="image" className="w-full h-full object-cover"/></div>
-            <div className="w-70 h-100 "><img src="/image (1).png" alt="image" className="w-full h-full object-cover"/></div>
-            <div className="w-70 h-90 image-right "><img src="/my photo solid 2.png" alt="image" className="w-full h-full object-cover"/></div>
+          <div className="relative  mr-80">
+            <div className="w-70 h-90 absolute inset-0 image-left ">
+              <img
+                src="/myphoto 1.png"
+                alt="image"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-70 h-90 absolute inset-0 image-right ">
+              <img
+                src="/my photo solid 2.png"
+                alt="image"
+                className="w-full h-full object-cover"
+              />
+            </div>
+                 <div className="w-70 h-100  absolute inset-0 image-middle">
+              <img
+                src="/image (1).png"
+                alt="image"
+                className="w-full h-full object-cover"
+              />
+            </div>
           </div>
         </div>
       </Container>
-      {children}
     </div>
+    <main>
+      {children}
+    </main>
+  </>
   );
 };
 export default PageTransition1;
